b4a796695e3f9f4afddd8084b9a9c8e3c93a0831
PoolFlamingo <pablolopez280797@gmail.com>
2026-04-09 16:55:09 +0200

    Add localization support for English and Spanish with JSON files

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,9 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { resources } from "./resources";
+import commonEs from "./locales/es/common.json";
+import todoEs from "./locales/es/todo.json";
+import commonEn from "./locales/en/common.json";
+import todoEn from "./locales/en/todo.json";
 
 i18n.use(initReactI18next).init({
-  resources,
+  resources: {
+    es: {
+      common: commonEs,
+      todo: todoEs,
+    },
+    en: {
+      common: commonEn,
+      todo: todoEn,
+    },
+  },
   lng: "es",
   fallbackLng: ["es"],
   ns: ["common", "todo"],
